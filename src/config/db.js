611cfb05fda785c2b0252a2135516f1fd62f8c0c
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2024-09-28 14:21:36 +0530

    db password added to env

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 import mysql2 from "mysql2";
-
+import dotenv from "dotenv";
+dotenv.config();
 const pool = mysql2.createPool({
     host: "localhost",
     user: "root",
-    password: "password",
+    password: process.env.DB_PASSWORD,
     database: "railway",
     port: 3306,
     waitForConnections: true,
